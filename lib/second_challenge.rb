986091fb2001826690ada5ec5d3b0ceb2554db3a
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-17 23:51:15 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,8 +2,8 @@ def second_challenge
   epic_tragedy = {
     
     :montague => {
-     :patriarch => {},
-     :matriarch => {},
+     :patriarch => {:name => "Lord Montague", :age => "53"},
+     :matriarch => {:name => "Lady Montague", :age => "54"},
      :hero => {},
      :hero_friends => {}
       }, 
