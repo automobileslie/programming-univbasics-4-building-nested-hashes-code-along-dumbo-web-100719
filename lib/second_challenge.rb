64de82ede5315495da1ce8e94f0e44decb30d1d5
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-17 23:39:51 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,13 @@
 def second_challenge
   epic_tragedy = {
+    
     :montague => {
      :patriarch => {},
      :matriarch => {},
      :hero => {},
-     :hero_friends
+     :hero_friends => {}
       }, 
+      
     :capulet => {
       :patriarch => {},
       :matriarch => {},
